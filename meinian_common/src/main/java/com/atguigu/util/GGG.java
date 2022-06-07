769dc1325882d76a64669aba7cb01c5717e14ae6
Chennner <100897298+Chennner@users.noreply.github.com>
2022-06-07 17:05:59 +0800

    Update GGG.java

--- meinian_common/src/main/java/com/atguigu/util/GGG.java
+++ meinian_common/src/main/java/com/atguigu/util/GGG.java
@@ -3,5 +3,6 @@ package com.atguigu.util;
 public class GGG {
     public static void main(String[] args) {
         System.out.println("ggggggggggggggggggg");
+        System.out.println("gggggggggdgft4ryt5r4ewy543uy54gggggggggg");
     }
 }
